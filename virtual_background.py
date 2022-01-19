7dd26931db19138255cbab8904d21a0d9d07d098
James <ji.devenport123@gmail.com>
2022-01-19 15:35:35 +0000

    Added new effects

--- virtual_background.py
+++ virtual_background.py
@@ -23,13 +23,15 @@ class Background:
         self.height = args.height
         self.background_image = cv2.resize(cv2.imread(args.background_image), (self.width, self.height))
 
-    def frame_with_background(self, current_frame):
+    def frame_with_background(self, current_frame, effects, frame_no):
         mask = generate_mask(current_frame)
         condition = np.stack((mask.segmentation_mask,) * 3, axis=-1) > 0.1
         if self.background_image is None:
             self.background_image = np.zeros(current_frame.shape, dtype=np.uint8)
             self.background_image[:] = BG_COLOR
-        masked_frame = np.where(condition, current_frame, self.background_image)
+        applicator = effects.apply_effects(current_frame, frame_no)
+        masked_frame = np.where(condition, applicator.current_frame, self.background_image)
+        masked_frame = cv2.cvtColor(masked_frame, cv2.COLOR_BGR2RGB)
         return masked_frame
 
 
@@ -62,11 +64,57 @@ class CameraOutput:
 class Effects:
 
     def __init__(self, args):
-        self.wave_distortion = args.wave_distortion
+        self.sepia = args.sepia
+        self.film_jitter = args.film_jitter
+        self.noise = args.noise
+        self.width = args.width
+        self.height = args.height
 
-    def apply_effects(self, current_frame):
-        if self.wave_distortion is True:
-            return
+    def apply_effects(self, current_frame, frame_no):
+        applicator = EffectApplicator(current_frame, frame_no, self.width, self.height)
+        if self.sepia is True:
+            applicator.sepia_applicator()
+        if self.film_jitter is True:
+            applicator.film_jittering_applicator()
+        if self.noise is True:
+            applicator.noise_applicator()
+        return applicator
+
+
+class EffectApplicator:
+
+    def __init__(self, current_frame, frame_no, width, height):
+        self.current_frame = current_frame
+        self.frame_no = frame_no
+        self.width = width
+        self.height = height
+
+    def sepia_applicator(self):
+        working_frame = self.current_frame
+        working_frame = np.array(working_frame, dtype=np.uint8)
+        working_frame = cv2.transform(working_frame, np.matrix([[0.272, 0.534, 0.131],
+                                                                [0.349, 0.686, 0.168],
+                                                                [0.393, 0.769, 0.189]]))
+        working_frame[np.where(working_frame > 255)] = 255
+        self.current_frame = working_frame
+
+    def film_jittering_applicator(self):
+        working_frame = self.current_frame
+        if self.frame_no % 2 == 0:
+            jitter_height = self.height / 160
+        if self.frame_no % 2 == 1:
+            jitter_height = -(self.height / 160)
+        translation = np.float32([[1, 0, 0], [0, 1, jitter_height]])
+        working_frame = cv2.warpAffine(working_frame, translation, (working_frame.shape[1], working_frame.shape[0]))
+        self.current_frame = working_frame
+
+    def noise_applicator(self):
+        working_frame = self.current_frame
+        row, col, ch = working_frame.shape
+        gauss = np.random.randn(row, col, ch).astype(np.uint8)
+        gauss = gauss.reshape(row, col, ch)
+        noisy = working_frame + working_frame * gauss
+        self.current_frame = noisy
 
 
 # Function to generate human outline mask over background
@@ -87,6 +135,7 @@ def str2bool(string):
         return False
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
+
 
 # Function to parse arguments from the command line into the script
 
@@ -115,11 +164,21 @@ def parse_args():
                         type=int,
                         default=30,
                         help='Frames per second for the webcam input.')
-    parser.add_argument('--wave_distortion',
+    parser.add_argument('--sepia',
                         type=str2bool,
                         nargs='?',
-                        default=True,
-                        help='Toggle a wave distortion effect.')
+                        default=False,
+                        help='Toggle a sepia effect.')
+    parser.add_argument('--film_jitter',
+                        type=str2bool,
+                        nargs='?',
+                        default=False,
+                        help='Toggle a film jittering effect.')
+    parser.add_argument('--noise',
+                        type=str2bool,
+                        nargs='?',
+                        default=False,
+                        help='Toggle a speckled noise effect.')
     return parser.parse_args()
 
 
@@ -129,9 +188,10 @@ args = parse_args()
 cam = CameraOutput(args)
 background = Background(args)
 effects = Effects(args)
+frame_counter = 0
 while cam.capture.isOpened():
     frame = cam.run()
-    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame_with_background = background.frame_with_background(rgb_frame)
-    effects.apply_effects(frame_with_background)
+    frame_no = frame_counter + 1
+    frame_counter = frame_no
+    frame_with_background = background.frame_with_background(frame, effects, frame_no)
     cam.output.schedule_frame(frame_with_background)
